d58f691178ee9aa2dfd9e48ee7df36ec3611b28d
fercholo0730 <fercholo0730@hotmail.com>
2021-10-04 21:41:07 -0500

    Clase 4 -oct 2021

---
+++ class16_JS_Intro/js/codigo.js
@@ -0,0 +1,24 @@
+var name = "Luis"
+console.log(name)
+
+let lastName = "Lopez"
+console.log(lastName)
+
+console.log(name + " " + lastName)
+
+let color = "blanco"
+    
+if (color == "negro"){
+    console.warn("NO! el color es " + color)
+}
+else if (color == "blanco")
+{
+    console.error("si! el color es" + color);
+}
+else
+{
+    console.error("You are lost")
+}
+
+
+
